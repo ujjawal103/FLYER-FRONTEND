ab600c30c59949192b57e58cc8be2ca2d92a84c9
ujjawal103 <ujjawaljaiswal618@gmail.com>
2025-08-31 09:58:21 +0530

    removed mongoose from frontend

--- src/dronePages/DroneUserSignup.jsx
+++ src/dronePages/DroneUserSignup.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useState } from 'react'
 import { Link , useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { UserDataContext } from '../dronecontext/DroneUserContext'
-import { set } from 'mongoose'
 import toast from 'react-hot-toast'
 
 const DroneUserSignup = () => {
